1c9d9369078ca3f0ba9de700c1b160bb643d605f
Julien Freyermuth <julien.chipster@gmail.com>
2015-11-09 16:51:18 +0100

    commit some shit part 2

--- static/app/module.js
+++ static/app/module.js
@@ -2,5 +2,5 @@ angular.module("appHabilitation", ["ui.router", "restangular", "ngMaterial"])
   .run(runBlock)
   .config(Config)
   .config(Route)
-  .controller("appHabilitationCtrl", LoginHabilitationCtrl)
+  .controller("loginHabilitationCtrl", LoginHabilitationCtrl)
   .controller("exemCtrl", ExemCtrl);
